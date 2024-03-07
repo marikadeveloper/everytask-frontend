84b5b12a89329b7f7d968d163098753110cf8190
Marika Mella <marika.developer@gmail.com>
2024-03-07 13:53:00 +0100

    Merged main into marika

--- src/authenticated-app.jsx
+++ src/authenticated-app.jsx
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import { ErrorBoundary } from 'react-error-boundary';
 import { Route, Routes } from 'react-router-dom';
 import {
@@ -6,6 +7,7 @@ import {
 } from './components/errors/index.jsx';
 import { DashboardScreen } from './screens/dashboard';
 import { NotFoundScreen } from './screens/not-found';
+import TasksScreen from './screens/tasks';
 
 function AuthenticatedApp() {
   return (
@@ -28,7 +30,10 @@ function AppRoutes() {
         path='/dashboard'
         element={<DashboardScreen />}
       />
-
+      <Route
+        path='/tasks'
+        element={<TasksScreen />}
+      />
       <Route
         path='/'
         element={<DashboardScreen />}
---
+++ src/screens/tasks/index.jsx
@@ -0,0 +1,9 @@
+function TasksScreen() {
+  return (
+    <div className='layout tasks'>
+      <h1>Tasks</h1>
+    </div>
+  );
+}
+
+export default TasksScreen;
