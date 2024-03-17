c1ec36807f9ba746962cf17472d4ec50d852e2db
AlbertoIT-23e <albertocozzarin@outlook.com>
2024-03-17 09:39:52 +0100

    Refactor profile screen component

--- src/screens/profile/index.jsx
+++ src/screens/profile/index.jsx
@@ -1,97 +1,94 @@
-import { Select, SelectItem, useDisclosure } from "@nextui-org/react";
-import { useForm } from "react-hook-form";
-import { Button } from "../../components/button";
-import { ChangePassword } from "../../components/changePassword";
-import { Input } from "../../components/input";
-import { useAuth } from "../../context/auth-context";
-import { dateFormats } from "../../utils/constants";
-import { useAsync } from "../../utils/hooks";
-import "./styles.scss";
-import { useUpdateUser } from "../../utils/user.js";
+import { Select, SelectItem } from '@nextui-org/react';
+import { useForm } from 'react-hook-form';
+import { Button } from '../../components/button';
+import { ChangePassword } from '../../components/change-password.jsx';
+import { Input } from '../../components/input';
+import { useAuth } from '../../context/auth-context';
+import { dateFormats } from '../../utils/constants';
+import { useAsync } from '../../utils/hooks';
+import { useUpdateUser } from '../../utils/user.js';
+import './styles.scss';
 
 function ProfileScreen() {
   const { user } = useAuth();
-  const { error, isLoading, isError, run } = useAsync();
-  const { test, handleSubmit, register } = useForm();
-  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+  const { isLoading } = useAsync();
+  const { handleSubmit, register } = useForm();
   const { mutate } = useUpdateUser();
-
 
   const onSubmit = (data) => {
     console.log(data);
+    mutate(data);
     /* 🤔 Alberto: 1. scrivi questo:
                           mutate(data);
     * così avrai finito la funzionalità di modifica dei dati del profilo :D
+    *
+    *  Attenzione: fatto ma adesso quando clicco save ricarica la pagina
+    * profile e compare Sorry.. nothing here. Go home. Quando vado alla home
+    * devo rifare il log in
     * */
   };
 
-  const modalOnSubmit = (data) => {
-    console.log("Current Password:", data.currentPassword);
-    console.log("New Password:", data.newPassword);
-    console.log("Confirm Password:", data.confirmPassword);
-  };
-
   return (
-    <div className="layout profile">
+    <div className='layout profile'>
       <form
         onSubmit={handleSubmit(onSubmit)}
-        className="profile__form"
+        className='profile__form'
       >
-        <header className="profile__form__header">
+        <header className='profile__form__header'>
           <h1>Profile</h1>
           <Button
             isLoading={isLoading}
-            className="profile__form__header__submit-save"
-            type="submit"
+            className='profile__form__header__submit-save'
+            type='submit'
           >
             Save
           </Button>
         </header>
-        <section className="profile__form__account-info">
+        <section className='profile__form__account-info'>
           <h2>Account information</h2>
           <Input
-            id="name"
-            type="text"
-            label="Name"
-            placeholder="Your name"
+            id='name'
+            type='text'
+            label='Name'
+            placeholder='Your name'
             defaultValue={user.name}
-            {...register("name")}
+            {...register('name')}
           />
           <Input
             isDisabled
-            id="email"
-            type="email"
-            label="Email"
-            placeholder="Your email"
+            id='email'
+            type='email'
+            label='Email'
+            placeholder='Your email'
             defaultValue={user.email}
-            {...register("email")}
+            {...register('email')}
           />
           <ChangePassword />
         </section>
-        <section className="profile__form__customization">
+        <section className='profile__form__customization'>
           <h2>Customization</h2>
           <Select
             items={dateFormats}
-            label="Date Format"
-            placeholder="Select a date format"
-            defaultSelectedKeys={["DD/MM/YYYY"]}
-            className="input"
-            variant={"bordered"}
-            color="default"
-            {...register("date")}
+            label='Date Format'
+            placeholder='Select a date format'
+            defaultSelectedKeys={['DD/MM/YYYY']}
+            className='input'
+            variant={'bordered'}
+            color='default'
+            {...register('date')}
           >
             {(dateFormat) => (
               <SelectItem key={dateFormat.value}>{dateFormat.label}</SelectItem>
             )}
           </Select>
         </section>
-        <section className="profile__form__danger-zone">
+        <section className='profile__form__danger-zone'>
           <h2>Danger zone</h2>
           <Button
-            className="profile__form__danger-zone__submit-delete"
-            type="button"
-            color="danger"
-            variant="bordered"
+            className='profile__form__danger-zone__submit-delete'
+            type='button'
+            color='danger'
+            variant='bordered'
           >
             Delete account
           </Button>
