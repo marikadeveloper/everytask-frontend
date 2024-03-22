54dfed37aedaba509702e06d8fbd60a3a56e3c64
Marika Mella <marika.developer@gmail.com>
2024-03-22 09:58:53 +0100

    Prepared route for the task detail

--- src/components/kanban-task/index.jsx
+++ src/components/kanban-task/index.jsx
@@ -1,57 +1,59 @@
+import { Link } from "react-router-dom";
+import { Emoji } from "emoji-picker-react";
 import PropTypes from "prop-types";
 import TaskImpactChip from "../task-impact-chip/index";
 import TaskDeadline from "../task-deadline/index";
 import { ListCircle } from "../../assets/icons/index";
 import { TASK_STATUS } from "../../utils/task";
 import "./styles.scss";
-// eslint-disable-next-line import/order
-import { Emoji } from "emoji-picker-react";
 
 function KanbanTask({ task, provided, snapshot }) {
   return (
-    <div
-      className={`kanban-task kanban-task--${task.status}`}
-      ref={provided.innerRef}
-      {...provided.draggableProps}
-      {...provided.dragHandleProps}
-      style={{
-        backgroundColor: snapshot.isDragging ? "white" : "white",
-        ...provided.draggableProps.style,
-      }}
-    >
-      <div className="kanban-task__title">
-        {task.emoji && (
-          <div className="kanban-task__emoji">
-            <Emoji size={20} lazyLoad unified={task.emoji} />
+    <Link className="kanban-task-link" to={`/tasks/${task.id}`}>
+      <div
+        className={`kanban-task kanban-task--${task.status}`}
+        ref={provided.innerRef}
+        {...provided.draggableProps}
+        {...provided.dragHandleProps}
+        style={{
+          backgroundColor: snapshot.isDragging ? "white" : "white",
+          ...provided.draggableProps.style,
+        }}
+      >
+        <div className="kanban-task__title">
+          {task.emoji && (
+            <div className="kanban-task__emoji">
+              <Emoji size={20} lazyLoad unified={task.emoji} />
+            </div>
+          )}
+          <p>{task.title}</p>
+        </div>
+        <div className="kanban-task__footer">
+          <div>
+            <TaskImpactChip impact={task.impact} iconOnly />
+            <TaskDeadline
+              deadline={task.dueDate}
+              isDone={task.status === TASK_STATUS.DONE}
+              short
+            />
+            {task.category && (
+              <p className="kanban-task__footer__category">
+                {task.category?.name}
+              </p>
+            )}
           </div>
-        )}
-        <p>{task.title}</p>
-      </div>
-      <div className="kanban-task__footer">
-        <div>
-          <TaskImpactChip impact={task.impact} iconOnly />
-          <TaskDeadline
-            deadline={task.dueDate}
-            isDone={task.status === TASK_STATUS.DONE}
-            short
-          />
-          {task.category && (
-            <p className="kanban-task__footer__category">
-              {task.category?.name}
-            </p>
+          {!!task.checklistItems?.length && (
+            <div className="kanban-task__footer__checklist">
+              <ListCircle />
+              <span>
+                {task.checklistItems.filter((item) => item.completed).length}/
+                {task.checklistItems.length}
+              </span>
+            </div>
           )}
         </div>
-        {!!task.checklistItems?.length && (
-          <div className="kanban-task__footer__checklist">
-            <ListCircle />
-            <span>
-              {task.checklistItems.filter((item) => item.completed).length}/
-              {task.checklistItems.length}
-            </span>
-          </div>
-        )}
       </div>
-    </div>
+    </Link>
   );
 }
 
