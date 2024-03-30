8fd3975a3635d5ddb759387906c3aba3937043ea
AlbertoIT-23e <albertocozzarin@outlook.com>
2024-03-30 10:27:22 +0100

    Refactor Badge component to use userBadge object

--- src/components/badge/index.jsx
+++ src/components/badge/index.jsx
@@ -1,17 +1,17 @@
 import PropTypes from "prop-types";
 import "./styles.scss";
 
-function Badge({ code }) {
+function Badge({ userBadge }) {
   return (
     <img
       className="badge"
-      src={`/src/assets/badges/${code}.jpg`}
-      alt={`${code} badge`}
+      src={`/src/assets/badges/${userBadge.badge.code}.jpg`}
+      alt={`${userBadge.badge.code} badge`}
     />
   );
 }
 Badge.propTypes = {
-  code: PropTypes.string.isRequired,
+  userBadge: PropTypes.object.isRequired,
 };
 
 export default Badge;
